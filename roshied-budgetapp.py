111bf58215a386d9c0255ad0c1357a93f63eda37
rasheed007 <69124494+rasheed007@users.noreply.github.com>
2021-04-20 21:12:50 +0100

    Update roshied-budgetapp.py
    This is my budget app model for zuri

--- roshied-budgetapp.py
+++ roshied-budgetapp.py
@@ -1,32 +1,38 @@
-class BudgetApp:
-    pass
+class Budget:
 
-    def _init_(self, category, amount):
-        self.category = category
-        self.amount = amount
+    def __init__(self,**categories):
+        self.categories = categories
+        print(self.categories)
 
-    #methods
-    def deposit(self):
-        return 'you just deposited'
+    def deposit(self,amount,category):
+        self.categories[category] = self.categories[category] + amount
+        print('You deposited the sum of $'+str(amount)+' '+ 'to '+category)
+    
+    def withdraw(self,amount,category):
+        if amount > self.categories[category]:
+            print("Insufficient Balance")
+        else:
+
+            self.categories[category] = self.categories[category] - amount
+            print('You withdrew the sum of $'+str(amount)+' from '+ category)
+
+    def transfer(self,amount,debitCategory,creditCategory):
+        if amount > self.categories[debitCategory]:
+            print('Insufficient Balance')
+        else:
+            self.categories[debitCategory] = self.categories[debitCategory] - amount
+            self.categories[creditCategory] =  self.categories[creditCategory] + amount
+            print('You transfered the sum of $'+ str(amount) + ' ' + 'from ' + debitCategory + ' to '+ creditCategory )
+    
+    def checkBalance(self,category):
+        print('Your '+ category + ' budget balance is ' + str(self.categories[category]))
         
 
-    def get_balance(self):
-        return 'you just get_balance'
-        pass
-
-    def withdraw(self):
-        return 'you just withdrew'
-        pass
-
-    def transfer(self):
-        return 'you just transfered'
-        pass
-
-
-category = BudgetApp('clothing', 5000)
-category_2 = BudgetApp('Food', 7000)
-category_3 = BudgetApp('Entertainment', 1000)
-category_4 = BudgetApp('Rent', 9000)
+mybudget= Budget(food=5000, clothing=3500, rent=9000, entertainment=4500, car_maintenance=3200)
+mybudget.deposit(1750,'food')
+mybudget.deposit(2500, 'car_maintenance')
+mybudget.withdraw(5600,'rent')
+mybudget.transfer(1000,'entertainment','clothing')
 
 
 
